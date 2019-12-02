d3d354c4e3e8029c886e980abfbd1f7c49a65dde
Bernardo Escoffié Torre <escoffie@think-comunicacion.com>
2019-12-02 12:46:19 -0700

    Instalación de WooCommerce

--- themes/floricel/functions-parts/theme-support.php
+++ themes/floricel/functions-parts/theme-support.php
@@ -3,6 +3,7 @@
 add_action('after_setup_theme', function () {
     add_theme_support('responsive-embeds');
     add_theme_support( 'title-tag' );
+    add_theme_support( 'woocommerce' );
 });
 
 add_action('init', function () {
